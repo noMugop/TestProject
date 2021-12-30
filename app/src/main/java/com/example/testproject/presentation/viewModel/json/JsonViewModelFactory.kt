2ac4c439882e25d1ab8c66a40b6ba06a9352c008
noMugop <nomugopradio@gmail.com>
2021-12-30 19:09:15 +0600

    ViewModelFactory changed

--- app/src/main/java/com/example/testproject/presentation/viewModel/json/JsonViewModelFactory.kt
+++ app/src/main/java/com/example/testproject/presentation/viewModel/json/JsonViewModelFactory.kt
@@ -3,13 +3,20 @@ package com.example.testproject.presentation.viewModel.json
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.testproject.domain.repository.json.useCase.GetGameInfoListUseCase
+import com.example.testproject.domain.repository.json.useCase.GetGameInfoUseCase
+import com.example.testproject.domain.repository.json.useCase.LoadDataUseCase
 import java.lang.RuntimeException
 
-class JsonViewModelFactory(private val application: Application): ViewModelProvider.Factory {
+class JsonViewModelFactory(
+    private val getGameInfoListUseCase: GetGameInfoListUseCase,
+    private val getGameInfoUseCase: GetGameInfoUseCase,
+    private val loadDataUseCase: LoadDataUseCase
+    ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(JsonViewModel::class.java)) {
-            return JsonViewModel(application) as T
+            return JsonViewModel(getGameInfoListUseCase, getGameInfoUseCase, loadDataUseCase) as T
         } else {
             throw RuntimeException("Wrong view model class $modelClass")
         }
